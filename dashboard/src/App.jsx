0133724206b827cd32e5c9ec29e3c5cc05a32119
mma-tahmid <mma.tahmid@gmail.com>
2025-07-28 12:40:55 +0600

    setup react & express js

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    
+    <>
+     
+
+<h1 className='bg-red-400'>  This is A Dashboard </h1>
+
+    </>
+  )
+}
+
+export default App
